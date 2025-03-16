8acc6acc5b9fe40900c155a4e9e636ff6e3f6a91
Eric Wu <118374162+Eric7895@users.noreply.github.com>
2025-03-16 17:55:13 -0400

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,25 @@
 from sqlalchemy import create_engine
+import os
 
+def get_user_and_password():
+    if os.path.exists('mysql_password.txt'):
+        with open('mysql_password.txt', 'r') as file:
+            for line in file:
+                if 'username' in line:
+                    username = line.split('=')[1].strip()
+                elif 'password' in line:
+                    password = line.split('=')[1].strip()
+
+        return username, password
+    else:
+        return 'File not found', 0
+    
 def get_engine():
-    username = 'Your username'
-    password = 'Your password'
+    username, password = get_user_and_password()
+
+    if username == 'File not found':
+        return 'Create mysql_password.txt first'
+
     host = 'localhost'  
     database = 'movie_API'
     return create_engine(f'mysql+pymysql://{username}:{password}@{host}/{database}')
